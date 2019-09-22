c689b13f3ab0aed95df444574cad5c486eb55bd5
David Brookes <david@davidbrookes.io>
2019-09-22 18:34:28 +0100

    initial add

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "airbnb",
+    "prettier",
+    "prettier/standard",
+    "prettier/react"
+  ],
+  plugins: ["react", "prettier"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  globals: {
+    Sentry: "readonly"
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js"] }],
+    "jsx-a11y/label-has-for": [
+      2,
+      {
+        required: { every: ["nesting"] },
+        allowChildren: true
+      }
+    ],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "react/state-in-constructor": "off",
+    "react/no-danger": "off",
+    "react/forbid-prop-types": "off",
+    "react/jsx-props-no-spreading": "off"
+  }
+};
